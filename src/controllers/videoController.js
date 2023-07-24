f2762bc3bb6f81b00c5dae1eb001eb9e8a47b2f3
uk <ukiiing00@gmail.com>
2023-07-24 21:40:38 +0900

    ROUTES: Router / Controller / Exports / Architecture

---
+++ src/controllers/videoController.js
@@ -0,0 +1,6 @@
+const trending = (req, res) => res.send('Home Page Video');
+const watch = (req, res) => res.send('Watch');
+const edit = (req, res) => res.send('Edit');
+
+export default trending;
+export { watch, edit };
--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import morgan from 'morgan';
+import globalRouter from './routers/globalRouter';
+import videoRouter from './routers/videoRouter';
+import userRouter from './routers/userRouter';
 
 const PORT = 4000;
 
@@ -8,24 +11,7 @@ const logger = morgan('dev'); // morgan() has 5 options
 
 app.use(logger);
 
-const globalRouter = express.Router(); // global
-
-const handleHome = (req, res) => res.send('Home');
-
-globalRouter.get('/', handleHome);
-
-const userRouter = express.Router(); // users
-
-const handleEditUser = (req, res) => res.send('Edit User');
-
-userRouter.get('/edit', handleEditUser);
-
-const videoRouter = express.Router(); // videos
-
-const handleWatchVideo = (req, res) => res.send('Watch Video');
-
-videoRouter.get('/watch', handleWatchVideo);
-
+// Router
 app.use('/', globalRouter);
 app.use('/videos', videoRouter);
 app.use('/users', userRouter);
