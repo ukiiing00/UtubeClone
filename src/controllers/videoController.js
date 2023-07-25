244d265ca84dd4bccea53ff576e9067162a907b7
uk <ukiiing00@gmail.com>
2023-07-26 08:55:44 +0900

    MONGODB AND MONGOOSE: ES6 const { id } = req.params;

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,34 +1,39 @@
+let videos = [
+    {
+        title: 'First Video',
+        rating: 5,
+        comments: 2,
+        createdAt: '2 minutes ago',
+        views: 59,
+        id: 1,
+    },
+    {
+        title: 'Second Video',
+        rating: 5,
+        comments: 2,
+        createdAt: '2 minutes ago',
+        views: 59,
+        id: 1,
+    },
+    {
+        title: 'Third Video',
+        rating: 5,
+        comments: 2,
+        createdAt: '2 minutes ago',
+        views: 59,
+        id: 1,
+    },
+];
+
 const trending = (req, res) => {
-    const videos = [
-        {
-            title: 'First Video',
-            rating: 5,
-            comments: 2,
-            createdAt: '2 minutes ago',
-            views: 59,
-            id: 1,
-        },
-        {
-            title: 'Second Video',
-            rating: 5,
-            comments: 2,
-            createdAt: '2 minutes ago',
-            views: 59,
-            id: 1,
-        },
-        {
-            title: 'Third Video',
-            rating: 5,
-            comments: 2,
-            createdAt: '2 minutes ago',
-            views: 59,
-            id: 1,
-        },
-    ];
     res.render('home', { pageTitle: 'Home', videos });
 };
-const see = (req, res) => res.render('watch', { pageTitle: 'Watch' });
-const edit = (req, res) => res.render('edit', { pageTitle: 'Edit' });
+const see = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render('watch', { pageTitle: `Watch ${video.title}` });
+};
+const edit = (req, res) => res.render('edit');
 const search = (req, res) => res.send('Search');
 const upload = (req, res) => res.send('Upload');
 const deleteVideo = (req, res) => {
