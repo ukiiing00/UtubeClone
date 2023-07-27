0ea078b52cf20d69ffad9c152f164de08805496f
uk <ukiiing00@gmail.com>
2023-07-28 08:50:53 +0900

    USER AUTHENTICATION: Form Validation / $or: [{ username }, { email }]

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,7 +4,23 @@ const getJoin = (req, res) =>
     res.render('join', { pageTitle: 'Create Account' });
 const postJoin = async (req, res) => {
     console.log(req.body);
-    const { name, username, email, password, location } = req.body;
+    const { name, username, email, password, password2, location } = req.body;
+    const exists = await User.exists({
+        $or: [{ username }, { email }],
+    });
+    if (exists) {
+        return res.render('join', {
+            pageTitle: 'Join',
+            errorMessage: 'This username/email is already taken',
+        });
+    }
+    if (password !== password2) {
+        return res.render('join', {
+            pageTitle: 'Join',
+            errorMessage: 'Password confirmation does not match.',
+        });
+    }
+
     await User.create({
         name,
         username,
