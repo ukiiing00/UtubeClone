5c03c44f8f072ae02cd5b0c1223b6e592d8a466c
uk <ukiiing00@gmail.com>
2023-07-27 18:48:51 +0900

    MONGODB AND MONGOOSE: Model.findByIdAndUpdate() / Model.exist()

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,3 @@
-import { error } from 'firebase-functions/logger';
 import Video from '../models/Video';
 
 const home = async (req, res) => {
@@ -9,19 +8,30 @@ const home = async (req, res) => {
 const watch = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
-    console.log(video);
+    if (!video) return res.render('404', { pageTitle: 'Video not found' });
     return res.render('watch', { pageTitle: video.title, video });
 };
-const getEdit = (req, res) => {
+const getEdit = async (req, res) => {
     const { id } = req.params;
-    res.render('edit', { pageTitle: `Editing:` });
+    const video = await Video.findById(id);
+    console.log(video);
+    if (!video) return res.render('404', { pageTitle: 'Video not found' });
+    return res.render('edit', { pageTitle: `Edit ${video.title}`, video });
 };
-const postEdit = (req, res) => {
+const postEdit = async (req, res) => {
     const { id } = req.params;
-    const { title } = req.body; // javascript representation
+    const { title, description, hashtags } = req.body;
+    const video = await Video.exists({ _id: id });
+    if (!video) return res.render('404', { pageTitle: 'Video not found' });
+    await Video.findByIdAndUpdate(id, {
+        title,
+        description,
+        hashtags: hashtags
+            .split(',')
+            .map((word) => (word.startsWith('#') ? word : `#${word}`)),
+    });
     return res.redirect(`/videos/${id}`);
 };
-
 const getUpload = (req, res) => {
     return res.render('upload', { pageTitle: `Upload Video` });
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -10,7 +10,7 @@ import {
 const videoRouter = express.Router(); // videos\
 
 videoRouter.get('/:id([0-9a-f]{24}$)', watch); // parameters + regular expresstion
-videoRouter.route('/:id([0-9][a-f]{24}$)/edit').get(getEdit).post(postEdit);
+videoRouter.route('/:id([0-9a-f]{24})/edit').get(getEdit).post(postEdit);
 videoRouter.route('/upload').get(getUpload).post(postUpload);
 
 export default videoRouter;
