ba485a5e3c04ce3c7e2b679eff1adf9e811a6a0d
uk <ukiiing00@gmail.com>
2023-07-29 15:29:58 +0900

    USER AUTHENTICATION: Sessions and Cookies / app.use(session({})) / req.sessionStore.all() / req,session

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@
 
 import express from 'express';
 import morgan from 'morgan';
+import session from 'express-session';
 import videoRouter from './routers/videoRouter';
 import userRouter from './routers/userRouter';
 import rootRouter from './routers/rootRouter';
@@ -19,6 +20,32 @@ app.set('x-powered-by', false);
 // middleware
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+//session
+app.use(
+    session({
+        secret: 'Hello!',
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+// app.use((req, res, next) => {
+//     console.log(req.headers);
+//     next();
+// });
+
+app.use((req, res, next) => {
+    req.sessionStore.all((error, sessions) => {
+        console.log(sessions);
+        next();
+    });
+});
+
+app.get('/add-one', (req, res, next) => {
+    req.session.tomato += 1;
+    return res.send(`${req.session.id} ${req.session.tomato}`);
+});
 
 // Router
 app.use('/', rootRouter);
