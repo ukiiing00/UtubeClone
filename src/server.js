9f2c5d46abbf0e7496f98dbfbcc59f27f66a36c9
uk <ukiiing00@gmail.com>
2023-07-31 18:26:23 +0900

    USER PROFILE: express.static();

--- src/server.js
+++ src/server.js
@@ -52,6 +52,7 @@ app.use((req, res, next) => {
 });
 
 // Router
+app.use('/uploads', express.static('uploads'));
 app.use('/', rootRouter);
 app.use('/videos', videoRouter);
 app.use('/users', userRouter);
