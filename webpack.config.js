5c91c4e462193fdfe9969a79814ae6f5536f1846
uk <ukiiing00@gmail.com>
2023-08-01 14:27:32 +0900

    WEBPACK: Better Developer Experience Settings

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,11 @@ module.exports = {
     entry: './src/client/js/main.js',
     plugins: [new MiniCssExtractPlugin({ filename: 'css/style.css' })],
     mode: 'development',
+    watch: true,
     output: {
         filename: 'js/main.js',
         path: path.resolve(__dirname, 'assets'),
+        clean: true,
     },
     module: {
         rules: [
